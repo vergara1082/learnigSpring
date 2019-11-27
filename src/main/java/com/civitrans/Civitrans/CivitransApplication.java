a4f9c5efb511070f3803fc084c730ff145c673fb
vergara1082 <jjvergara79@misena.edu.co>
2019-11-27 10:44:59 -0500

    aprendiendo spring

---
+++ src/main/java/com/civitrans/Civitrans/CivitransApplication.java
@@ -0,0 +1,14 @@
+package com.civitrans.Civitrans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivitransApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CivitransApplication.class, args);
+                
+	}
+
+}
---
+++ src/test/java/com/civitrans/Civitrans/CivitransApplicationTests.java
@@ -0,0 +1,13 @@
+package com.civitrans.Civitrans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivitransApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
